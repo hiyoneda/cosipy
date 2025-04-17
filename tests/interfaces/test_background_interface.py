d358b73832b1962e6d7127dc45b81607e8630b36
Israel Martinez <imc@umd.edu>
2025-04-17 09:01:01 -0400

    Null background no longer exists
    Signed-off-by: Israel Martinez <imc@umd.edu>

--- tests/interfaces/test_background_interface.py
+++ tests/interfaces/test_background_interface.py
@@ -1,21 +1,5 @@
-from cosipy.interfaces import (NullBackground,
-                               BackgroundInterface,
+from cosipy.interfaces import (BackgroundInterface,
                                BinnedBackgroundInterface,
                                UnbinnedBackgroundInterface
                                )
 
-def test_null_background():
-    null_1 = NullBackground()
-    null_2 = NullBackground
-    null_3 = NullBackground
-
-    assert null_1 is null_2
-    assert null_2 is null_3
-    assert null_3 is null_1
-    assert isinstance(null_1, BackgroundInterface)
-    assert isinstance(null_2, BinnedBackgroundInterface)
-    assert isinstance(null_3, UnbinnedBackgroundInterface)
-
-    class RandomClass: pass
-
-    assert not isinstance(null_1, RandomClass)
