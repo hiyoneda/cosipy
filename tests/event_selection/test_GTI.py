e569e6a08c1ea8a99be8fca6a0194ce81f08de40
Hiroki Yoneda <hiroki.yoneda.phys@gmail.com>
2025-10-22 12:17:34 -0500

    Modified the unittest for GTI

--- tests/event_selection/test_GTI.py
+++ tests/event_selection/test_GTI.py
@@ -5,14 +5,21 @@ from astropy.time import Time
 from cosipy.event_selection import GoodTimeInterval
 
 def test_GTI(tmp_path):
+    # test with 1 range
+    gti = GoodTimeInterval(Time(60970.0, format='mjd', scale = 'utc'),
+                           Time(60975.0, format='mjd', scale = 'utc'))
 
+    assert len(gti) == 1
+    
+    # test with 2 ranges
     tstarts = Time([60970.0, 60980.0], format='mjd', scale = 'utc')
     tstops  = Time([60975.0, 60985.0], format='mjd', scale = 'utc')
 
     gti = GoodTimeInterval(tstarts, tstops)
 
     assert len(gti) == 2
-
+    
+    # check the values
     tstarts = gti.tstart_list
     tstops  = gti.tstop_list
 
@@ -52,3 +59,14 @@ def test_GTI(tmp_path):
 
     assert np.all(gti_intersection.tstart_list == Time([60972.0, 60980.0], format='mjd', scale = 'utc'))
     assert np.all(gti_intersection.tstop_list  == Time([60975.0, 60983.0], format='mjd', scale = 'utc'))
+
+    #Intersection with no components
+    #GTI4
+    tstarts_4 = Time([60950.0], format='mjd', scale = 'utc')
+    tstops_4  = Time([60960.0], format='mjd', scale = 'utc')
+    
+    gti4 = GoodTimeInterval(tstarts_4, tstops_4)
+
+    gti_intersection_no_components = GoodTimeInterval.intersection(gti1, gti4)
+
+    assert len(gti_intersection_no_components) == 0
