cb472b4c2d76303b08eb41fe0012dd35d3787c6f
Jeremy Buhler <jbuhler@cassini.seas.wustl.edu>
2025-10-25 11:21:50 -0500

    merge numba-accelerated fast norm fit code

--- cosipy/ts_map/fast_norm_fit.py
+++ cosipy/ts_map/fast_norm_fit.py
@@ -1,27 +1,28 @@
-
 import numpy as np
 
-from numpy import log, sqrt, sqrt
-from math import factorial
+from numba import jit, prange
+
+import logging
+logger = logging.getLogger(__name__)
 
 class FastNormFit:
     """
-    Perform a fast poisson maximum likelihood ratio fit of the normalization of a 
+    Perform a fast poisson maximum likelihood ratio fit of the normalization of a
     source over background.
 
     The likelihood ratio as a function of the norm is computed as follow
 
     .. math::
-    
+
         TS(N) = 2 \\sum_i \\left( \\frac{\\log P(d_i; b_i+N e_i)}{\\log P(d_i; b_i)} \\right)
 
     where :math:`P(d; \lambda)` is the Poisson probability of obtaining :math:`d`
     count where :math:`\lambda` is expected on average; :math:`b` is the estimated
-    number of background counts; :math:`N` is the normalization; and :math:`e` 
-    is the expected excess -i.e. signal- per normalization unit -i.e. the 
-    number of excess counts equals :math:`N`. 
+    number of background counts; :math:`N` is the normalization; and :math:`e`
+    is the expected excess -i.e. signal- per normalization unit -i.e. the
+    number of excess counts equals :math:`N`.
 
-    It can be shown that :math:`TS(N)` has analytic derivative of arbitrary 
+    It can be shown that :math:`TS(N)` has analytic derivative of arbitrary
     order and that the Newton's method is guaranteed to converge if initialized
     at :math:`N=0`.
 
@@ -32,22 +33,22 @@ class FastNormFit:
         background itself (i.e. :math:`N_{B}/N_{off} \\lll 1`).
 
     .. note::
-    
+
         Because of the Poisson probability, :math:`TS(N)` is only well-defined
-        for :math:`N \geq 1`. By default, underfluctuations are set to 
+        for :math:`N \geq 1`. By default, underfluctuations are set to
         :math:`TS(N=0) = 0`. For cases when there is benefit in letting the
-        normalization float to negative values, you can use `allow_negative`, 
+        normalization float to negative values, you can use `allow_negative`,
         but in that case the results are only valid in the Gaussian regime.
 
     Args:
         max_iter (int): Maximum number of iteration
         conv_frac_tol (float): Convergence stopping condition, expressed as the
             ratio between the size of the last step and the current norm value.
-        zero_ts_tol (float): If zero_ts_tol < TS < 0, then TS is set to 0 
+        zero_ts_tol (float): If zero_ts_tol < TS < 0, then TS is set to 0
             without failed flag status (analytically, TS < 0 should never happen)
-        allow_negative (bool): Allow the normalization to float toward 
+        allow_negative (bool): Allow the normalization to float toward
             negative values
-    """    
+    """
 
     def __init__(self,
                  max_iter = 1000,
@@ -61,52 +62,60 @@ class FastNormFit:
         self.allow_negative = allow_negative
 
     @staticmethod
-    def ts(data, bkg, unit_excess, norm):
+    @jit(nopython=True, nogil=True, fastmath=True, parallel=True)
+    def ts(data, bkg, unit_excess, ue_sum, norm):
         """
-        Get TS for a given normalization. 
+        Get TS for a given normalization.
 
         Args:
             data (array): Observed counts
-            bkg (array): Background estimation. Same size as data. 
+            bkg (array): Background estimation. Same size as data.
                 Every element should be >0
-            unit_excess (array): Expected excess per normalization unit. 
+            unit_excess (array): Expected excess per normalization unit.
                 Same size as data.
             norm (float or array): Normalization value
 
         Return:
-            float or array: TS, same shape as norm
+            float: TS
         """
-        
-        data = np.array(data).ravel()
-        bkg = np.array(bkg).ravel()
-        unit_excess = np.array(unit_excess).ravel()
-        
-        ts = 0 if np.isscalar(norm) else np.zeros(np.shape(norm))
 
-        for d,b,ue in zip(data, bkg, unit_excess): 
+        ts = - norm * ue_sum
 
-            e = norm*ue
+        # NB: on Intel processors, np.log1p, like most
+        # transcendentals, uses a much slower implementation in Numba
+        # than in regular Numpy. Install the icc_rt package and make
+        # sure Numba has SVML support enabled (run numba -s to check)
+        # to fix.
+        for i in prange(len(data)):
+            ts += data[i] * np.log1p(norm * unit_excess[i]/bkg[i])
 
-            if d == 0:
-                ts -= e
-            else:
-                ts += d * log((b+e) / b) - e
- 
-        ts *= 2
-     
-        return ts
+        return 2 * ts
 
+    '''
     @staticmethod
-    def dts(data, bkg, unit_excess, norm, order=1):
+    def ts_serial(data, bkg, unit_excess, ue_sum, norm):
         """
-        Get the derivative of TS with respecto to the normalization,
-        at given normalization. 
+        Non-Numba impl of ts()
+        """
+
+        ts = np.sum(data * np.log1p(norm * unit_excess/bkg))
+
+        return 2 * (ts - norm * ue_sum)
+    '''
+
+    '''
+    @staticmethod
+    def dts(data, bkg, unit_excess, ue_sum, norm, order=1):
+        """
+        Get the derivative of TS with respect to the normalization,
+        at given normalization.  (Note: this function is replaced
+        with various accelerated versions in solve() below.)
 
         Args:
             data (array): Observed counts
-            bkg (array): Background estimation. Same size as data. 
+            bkg (array): Background estimation. Same size as data.
                 Every element should be >0
-            unit_excess (array): Expected excess per normalization unit. 
+            unit_excess (array): Expected excess per normalization unit.
                 Same size as data.
             norm (float or array): Normalization value
             order (int): Derivative order
@@ -114,130 +123,243 @@ class FastNormFit:
         Return:
             float or array: d^n TS / dN^n, same shape as norm
         """
+        from math import factorial
 
-        if order < 1:
-            raise ValueError("Order must be > 1")
-        
-        data = np.array(data).ravel()
-        bkg = np.array(bkg).ravel()
-        unit_excess = np.array(unit_excess).ravel()
+        assert order >= 1
 
-        dts = 0 if np.isscalar(norm) else np.zeros(np.shape(norm))
+        r = unit_excess / (bkg + norm * unit_excess)
 
-        for d,b,ue in zip(data, bkg, unit_excess): 
-
-            e = norm*ue
-
-            if d > 0:
-                dts += d * (ue/(b+e))**order
+        dts = np.sum(data * r**order)
 
         if order == 1:
-
-            for ue in unit_excess: 
-        
-                dts -= ue
+            dts -= ue_sum
 
         dts *= 2 * ((-1)**(order-1)) * factorial(order-1)
 
         return dts
-        
+    '''
+
+    @staticmethod
+    @jit(nopython=True, nogil=True, fastmath=True, parallel=True)
+    def d1ts(data, bkg, unit_excess, ue_sum, norm):
+        """
+        Get the first derivative of TS with respect to the normalization,
+        at given normalization.
+
+        Equivalent to dts(data, bkg, unit_excess, ue_sum, norm, order=1)
+
+        Use Numba to avoid allocating intermediate arrays.
+
+        """
+
+        f = -ue_sum
+
+        for i in prange(len(data)):
+            r = unit_excess[i] / (bkg[i] + norm * unit_excess[i])
+
+            dxr = data[i] * r
+            f += dxr
+
+        return 2*f
+
+    @staticmethod
+    @jit(nopython=True, nogil=True, fastmath=True, parallel=True)
+    def d2ts(data, bkg, unit_excess, norm):
+        """
+        Get the first derivative of TS with respect to the normalization,
+        at given normalization.
+
+        Equivalent to dts(data, bkg, unit_excess, ue_sum, norm, order=2)
+
+        Use Numba to avoid allocating intermediate arrays.
+
+        """
+
+        fp = data.dtype.type(0)
+
+        for i in prange(len(data)):
+            r = unit_excess[i] / (bkg[i] + norm * unit_excess[i])
+
+            dxr = data[i] * r * r
+            fp += dxr
+
+        return -2*fp
+
+    @staticmethod
+    @jit(nopython=True, nogil=True, fastmath=True, parallel=True)
+    def halley_stepsize(data, bkg, unit_excess, ue_sum, norm):
+        """
+        Step size for Halley's method.  Equivalent to
+
+        f = self.dts(data, bkg, unit_excess, ue_sum, norm, 1)
+        fp = self.dts(data, bkg, unit_excess, ue_sum, norm, 2)
+        fpp = self.dts(data, bkg, unit_excess, ue_sum, norm, 3)
+
+        -2 * f * fp / (2 * fp * fp - f * fpp)
+
+        Use Numba to avoid allocating intermediate arrays.
+
+        """
+
+        f = -ue_sum
+        fp = data.dtype.type(0)
+        fpp = data.dtype.type(0)
+
+        for i in prange(len(data)):
+            r = unit_excess[i] / (bkg[i] + norm * unit_excess[i])
+
+            dxr = data[i] * r
+            f += dxr
+            dxr *= r
+            fp += dxr
+            dxr *= r
+            fpp += dxr
+
+        return (f * fp)/(fp**2 - f * fpp)
+
+    '''
+    @staticmethod
+    @jit(nopython=True, nogil=True, fastmath=True, parallel=True)
+    def newton_stepsize(data, bkg, unit_excess, ue_sum, norm):
+        """
+        Step size for Newton's method.  Equivalent to
+
+        f = self.dts(data, bkg, unit_excess, ue_sum, norm, 1)
+        fp = self.dts(data, bkg, unit_excess, ue_sum, norm, 2)
+
+        -f/fp
+
+        Use Numba to avoid allocating intermediate arrays.
+
+        """
+
+        f  = -ue_sum
+        fp = data.dtype.type(0)
+
+        for i in prange(len(data)):
+            r = unit_excess[i] / (bkg[i] + norm * unit_excess[i])
+
+            dxr = data[i] * r
+            f += dxr
+            dxr *= r
+            fp += dxr
+
+        return f/fp
+    '''
+
+    @staticmethod
+    @jit(nopython=True, nogil=True, fastmath=True, parallel=True)
+    def newton_loop(data, bkg, unit_excess, ue_sum, max_iter, conv_frac_tol):
+
+        norm = data.dtype.type(0)
+        conv = False
+
+        for iteration in range(max_iter):
+            f  = -ue_sum
+            fp = data.dtype.type(0)
+
+            for i in prange(len(data)):
+                r = unit_excess[i] / (bkg[i] + norm * unit_excess[i])
+
+                dxr = data[i] * r
+                f += dxr
+                dxr *= r
+                fp += dxr
+
+            step = f/fp
+            norm += step
+
+            if step < norm * conv_frac_tol:
+                conv = True
+                break
+
+        return norm, conv, iteration
+
     def solve(self, data, bkg, unit_excess):
         """
-        Get the maximum TS, fitted normalization and normalization error 
+        Get the maximum TS, fitted normalization and normalization error
         (:math:`\Delta TS = 1`)
 
         .. note::
 
-            The normalization error is obtained from approximating the TS 
+            The normalization error is obtained from approximating the TS
             function as as parabola (i.e. valid in the Gaussian regime). TS
             and norm are indeed valid in the Poisson regime.
 
         Args:
             data (array): Observed counts
-            bkg (array): Background estimation. Same size as data. 
-                Every element should be >0
-            unit_excess (array): Expected excess per normalization unit. 
+            bkg (array): Background estimation. Same size as data.
+            unit_excess (array): Expected excess per normalization unit.
                 Same size as data.
 
         Return:
-            (float, float, float, bool): ts, norm, norm error and status (0 = good) 
+            (float, float, float, bool): ts, norm, norm error and status (0 = good)
+
         """
-        
-        dts0 = self.dts(data, bkg, unit_excess, 0)
+
+        ue_sum = np.sum(unit_excess)
+
+        # do not process CDS bins with no observed counts (does not
+        # impact result) or no bg counts (model issue)
+        valid = np.logical_and(data > 0, bkg > 0)
+
+        data = data[valid]
+        bkg = bkg[valid]
+        unit_excess = unit_excess[valid]
+
+        norm = data.dtype.type(0)
+
+        dts0 = self.d1ts(data, bkg, unit_excess, ue_sum, norm)
 
         if dts0 < 0:
             # Underfluctuation
 
             # A negative norm is not well defined since we use a
-            # Poisson Likelihood. When negative norm is allowed we assume
-            # that we are in the Gaussian regime
+            # Poisson Likelihood. When negative norm is allowed, we
+            # assume that we are in the Gaussian regime
 
-            ddts0 = self.dts(data, bkg, unit_excess, 0, order = 2)
+            ddts0 = self.d2ts(data, bkg, unit_excess, norm)
 
             if self.allow_negative:
+                ts   = 0.5 * dts0 * dts0 / ddts0
+                norm = -dts0 / ddts0
+                norm_err = np.sqrt(-2 / ddts0)
 
-                ts = dts0*dts0/2/ddts0
-
-                norm = -dts0/ddts0
-
-                norm_err = sqrt(-2/ddts0)
-            
             else:
-
                 ts = 0
                 norm = 0
-
                 if ddts0 == 0:
-
                     norm_err = -1/dts0
-                
                 else:
+                    norm_err = -(np.sqrt(dts0*dts0 - 2*ddts0) + dts0) / ddts0
 
-                    norm_err = -(sqrt(dts0*dts0 - 2*ddts0) + dts0) / ddts0
-
-            #Never failed, results where analytical
-            failed = False
-            
-            return (ts, norm, norm_err, failed)
+            # never failed, results were analytical
+            return (ts, norm, norm_err, False)
 
         # Newton's method
+        norm, conv, n_iters = self.newton_loop(data, bkg, unit_excess, ue_sum,
+                                               self.max_iter, self.conv_frac_tol)
 
-        conv = False
-        norm = 0
-        iteration = 0
-        
-        while (not conv and iteration < self.max_iter):
-
-            step = (-self.dts(data, bkg, unit_excess, norm, order = 1) /
-                    self.dts(data, bkg, unit_excess, norm, order = 2)
-)
-        
-            norm += step
-
-            if step/norm < self.conv_frac_tol:
-                conv = True
-
-            iteration += 1
-
-        # One extra step using Halley's method to avoid being biased toward 
-            
-        f = self.dts(data, bkg, unit_excess, norm,1)
-        fp = self.dts(data, bkg, unit_excess, norm,2)
-        fpp = self.dts(data, bkg, unit_excess, norm,3)
-        norm -= 2 * f * fp / (2 * fp * fp - f * fpp)
+        # One extra step using Halley's method to avoid bias
+        norm += self.halley_stepsize(data, bkg, unit_excess, ue_sum, norm)
 
         # Compute ts, norm error and checks
+        ts = self.ts(data, bkg, unit_excess, ue_sum, norm)
 
-        ts = self.ts(data, bkg, unit_excess, norm)
+        # estimated error in norm
+        norm_err = np.sqrt(-2/self.d2ts(data, bkg, unit_excess, norm))
 
-        norm_err = sqrt(-2/self.dts(data, bkg, unit_excess, norm, 2))
+        # sanity checks
+        failed = (not conv or norm < 0)
+        if failed:
+            logger.warning("Failed to converge")
 
-        failed = (norm < 0  or iteration == self.max_iter)
+        if ts < 0:
+            if ts < -self.zero_ts_tol:
+                failed = True
+                logger.warning("Failed -- ts is negative")
+            else:
+                #Assumed to be a numerical error
+                ts = 0
 
-        if ts < -self.zero_ts_tol:
-            failed = True
-        elif ts < 0:
-            #Assumed to be a numerical error
-            ts = 0
-
-        return (ts, norm, norm_err, failed, iteration)
+        return (ts, norm, norm_err, failed, n_iters)
