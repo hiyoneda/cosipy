110439c6c90cf7f579024d941948b40234c504d7
Hiroki Yoneda <hiroki.yoneda.phys@gmail.com>
2025-11-07 15:14:34 +0900

    Modified the unittest for the ccm and exposure table

--- tests/image_deconvolution/test_exposure_table.py
+++ tests/image_deconvolution/test_exposure_table.py
@@ -14,21 +14,21 @@ def test_exposure_table(tmp_path):
 
     ori = SpacecraftFile.parse_from_file(test_data.path / "20280301_first_10sec.ori")
 
-    assert SpacecraftAttitudeExposureTable.analyze_orientation(ori, nside=nside, start=None, stop=ori.get_time()[-1], min_exposure=0, min_num_pointings=1) == None
+    assert SpacecraftAttitudeExposureTable.analyze_orientation(ori, nside=nside, start=None, stop=ori.get_time()[-1], min_livetime=0, min_num_pointings=1) == None
 
-    assert SpacecraftAttitudeExposureTable.analyze_orientation(ori, nside=nside, start=ori.get_time()[0], stop=None, min_exposure=0, min_num_pointings=1) == None
+    assert SpacecraftAttitudeExposureTable.analyze_orientation(ori, nside=nside, start=ori.get_time()[0], stop=None, min_livetime=0, min_num_pointings=1) == None
 
     exposure_table = SpacecraftAttitudeExposureTable.from_orientation(ori, nside=nside, 
                                                                       start=ori.get_time()[0], stop=ori.get_time()[-1], 
-                                                                      min_exposure=0, min_num_pointings=1)
+                                                                      min_livetime=0, min_num_pointings=1)
 
     exposure_table_nest = SpacecraftAttitudeExposureTable.from_orientation(ori, nside=nside, scheme = 'nested',
                                                                            start=ori.get_time()[0], stop=ori.get_time()[-1], 
-                                                                           min_exposure=0, min_num_pointings=1)
+                                                                           min_livetime=0, min_num_pointings=1)
 
     exposure_table_badscheme = SpacecraftAttitudeExposureTable.from_orientation(ori, nside=nside, scheme = None,
                                                                                 start=ori.get_time()[0], stop=ori.get_time()[-1], 
-                                                                                min_exposure=0, min_num_pointings=1)
+                                                                                min_livetime=0, min_num_pointings=1)
 
     exposure_table.save_as_fits(tmp_path / "exposure_table_test_nside1_ring.fits")
     
@@ -36,7 +36,7 @@ def test_exposure_table(tmp_path):
 
     map_pointing_zx = exposure_table.calc_pointing_trajectory_map()
 
-    assert map_pointing_zx == Histogram.open(test_data.path / "image_deconvolution/map_pointing_zx_test_nside1_ring.hdf5")
+    assert np.all(map_pointing_zx.contents == Histogram.open(test_data.path / "image_deconvolution/map_pointing_zx_test_nside1_ring.hdf5").contents)
 
     # test_generating_histogram 
     full_detector_response = response.FullDetectorResponse.open(test_data.path / "test_full_detector_response.h5")
