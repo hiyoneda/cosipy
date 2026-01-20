0f8d8f9d1df65d9976bc648f211fbd77310ba8bc
GallegoSav <123730578+GallegoSav@users.noreply.github.com>
2026-01-20 18:10:54 +0100

    Update UnBinnedData.py

--- cosipy/data_io/UnBinnedData.py
+++ cosipy/data_io/UnBinnedData.py
@@ -423,7 +423,16 @@ class UnBinnedData(DataIO):
                         'Distance':dist,
                         'Chi galactic':chi_gal,
                         'Psi galactic':psi_gal,
-                       'Compton Seq':CO_seq} 
+                       'Compton Seq':CO_seq}
+                
+        #For simulation the timetags are shuffle so we need to sort it by ascending order
+        # Build sorting index
+        idx = np.argsort(cosi_dataset["TimeTags"])
+
+        # Reorder all columns using the same index
+        for key in cosi_dataset:
+            cosi_dataset[key] = unbinned_signal.cosi_dataset[key][idx]        
+        
         self.cosi_dataset = cosi_dataset
 
         # Option to write unbinned data to file (either fits or hdf5):
