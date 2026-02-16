2fb283592bd933054f445358f7443d12862968db
Israel Martinez <imc@umd.edu>
2026-02-16 10:57:26 -0500

    Improve handling of tests variable. Maybe fixing some issues as well.
    Signed-off-by: Israel Martinez <imc@umd.edu>

--- tests/threeml/test_spectral_fitting.py
+++ tests/threeml/test_spectral_fitting.py
@@ -20,22 +20,6 @@ data_path = test_data.path
 sc_orientation = SpacecraftHistory.open(data_path / "20280301_2s.ori")
 dr_path = str(data_path / "test_full_detector_response.h5") # path to detector response
 
-bkg_par_value = 1
-bkg_par = Parameter("background_cosi",                                         # background parameter
-                    bkg_par_value,                                             # initial value of parameter
-                    min_value=0,                                               # minimum value of parameter
-                    max_value=50,                                              # maximum value of parameter
-                    delta=0.05,                                                # initial step used by fitting engine
-                    desc="Background parameter for cosi")
-
-# second copy for testing with ICRS source
-bkg_par_icrs = Parameter("background_cosi_icrs",                                    # background parameter
-                         bkg_par_value,                                                         # initial value of parameter
-                         min_value=0,                                               # minimum value of parameter
-                         max_value=50,                                              # maximum value of parameter
-                         delta=0.05,                                                # initial step used by fitting engine
-                         desc="Background parameter for cosi")
-
 l = 50
 b = -45
 
@@ -67,25 +51,22 @@ def test_point_source_spectral_fit(background=None):
                                         scatt_map=sc_orientation.get_scatt_map(fdr.nside * 2, # Use same nside as hardcoded in COSILke
                                                                                source.position.sky_coord))
 
-    data = psr.get_expectation(source.spectrum.main.shape)
-    bkg = data.copy()
-    bkg[:] = np.mean(bkg)  # Flat background
-    data += bkg_par.value * bkg
+    data_dist = psr.get_expectation(source.spectrum.main.shape)
+    bkg_dist = data_dist.copy()
+    bkg_dist[:] = np.mean(bkg_dist)  # Flat background
+    data_dist += bkg_dist
+
+    bkg_rate = np.sum(bkg_dist) / sc_orientation.cumulative_livetime()
 
     dr = FullDetectorResponse.open(dr_path)
     instrument_response = BinnedInstrumentResponse(dr)
-
-    # Workaround to avoid inf values. Out bkg should be smooth, but currently it's not.
-    # Reproduces results before refactoring. It's not _exactly_ the same, since this fudge value was 1e-12, and
-    # it was added to the expectation, not the normalized bkg
-    bkg_dist_proj = bkg + sys.float_info.min
 
     # Move initial guess slightly away from true values
     spectrum.index.value = index * 1.1
     spectrum.K.value = K.value * 1.1
 
-    data = EmCDSBinnedData(data + bkg)
-    bkg = FreeNormBinnedBackground(bkg_dist_proj,
+    data = EmCDSBinnedData(data_dist)
+    bkg = FreeNormBinnedBackground(bkg_dist,
                                    sc_history=sc_orientation,
                                    copy=False)
 
@@ -105,6 +86,15 @@ def test_point_source_spectral_fit(background=None):
                                   response,
                                   bkg)
 
+
+    cosi.bkg_parameter['bkg_norm'] = Parameter("bkg_norm",  # background parameter
+                                               1.1 * bkg_rate.to_value(u.Hz),  # initial value of parameter
+                                               unit=u.Hz,
+                                               min_value=0,  # minimum value of parameter
+                                               max_value=1e6,  # maximum value of parameter
+                                               delta=1e5,  # initial step used by fitting engine
+                                               )
+
     plugins = DataList(cosi)
 
     model = Model(source)
@@ -115,8 +105,8 @@ def test_point_source_spectral_fit(background=None):
 
     sp = source.spectrum.main.shape
 
-    assert np.allclose([sp.K.value, sp.index.value, bkg_par.value],
-                       [K.value, index, bkg_par_value])
+    assert np.allclose([sp.K.value, sp.index.value, cosi.bkg_parameter['bkg_norm'].value],
+                       [K.value, index, bkg_rate.value])
 
     TS_ref = 6377269.127606418
 
@@ -144,6 +134,14 @@ def test_point_source_spectral_fit(background=None):
                                   response,
                                   bkg)
 
+    cosi.bkg_parameter['bkg_norm'] = Parameter("bkg_norm",  # background parameter
+                                               1.1 * bkg_rate.to_value(u.Hz),  # initial value of parameter
+                                               unit=u.Hz,
+                                               min_value=0,  # minimum value of parameter
+                                               max_value=1e6,  # maximum value of parameter
+                                               delta=1e5,  # initial step used by fitting engine
+                                               )
+
     plugins = DataList(cosi)
 
     like = JointLikelihood(model, plugins, verbose=False)
@@ -154,8 +152,8 @@ def test_point_source_spectral_fit(background=None):
     sp_icrs = source_icrs.spectrum.main.shape
 
     # make sure result does not change (much -- bkg_par changes more than the rest)
-    assert np.allclose([sp_icrs.K.value, sp_icrs.index.value, bkg_par_icrs.value],
-                       [K.value, index, bkg_par_value])
+    assert np.allclose([sp.K.value, sp.index.value, cosi.bkg_parameter['bkg_norm'].value],
+                       [K.value, index, bkg_rate.value])
 
     assert np.allclose([cosi.get_log_like()],
                        [TS_ref])
