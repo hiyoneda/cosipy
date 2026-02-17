86cab4ca49fdbd8530fed1819520cacd8c73aaa5
nmik <michelanegro@lsu.edu>
2026-02-17 12:28:50 -0600

    added more testing

--- tests/polarization/test_polarization_stokes.py
+++ tests/polarization/test_polarization_stokes.py
@@ -3,7 +3,7 @@ from astropy.coordinates import Angle, SkyCoord
 from astropy import units as u
 from scoords import SpacecraftFrame
 
-from cosipy.polarization.polarization_stokes import PolarizationStokes
+from cosipy.polarization.polarization_stokes import PolarizationStokes, rotate_points_to_x_axis
 from cosipy.spacecraftfile import SpacecraftFile
 from cosipy import UnBinnedData
 from cosipy.threeml.custom_functions import Band_Eflux
@@ -43,11 +43,17 @@ def test_stokes_polarization():
     
     average_mu = source_photons._mu100['mu']
     mdp99 = source_photons._mdp99
+    bkg_duration = source_photons.get_background_duration()
+    print('Bkg duration (should be 0):', bkg_duration)
 
     qs, us = source_photons.compute_data_pseudo_stokes(show_plots=False)
     polarization = source_photons.calculate_polarization(qs, us, average_mu, mdp=mdp99,
                                                          bkg_qs=None, bkg_us=None, show_plots=False)
     Pol_frac = polarization['fraction'] * 100
     Pol_angl = polarization['angle'].angle.degree
+
+    test_pd, test_pa = 0.8, 90
+    test_q, test_u = rotate_points_to_x_axis(test_pd, np.radians(test_pa))
+    print('Testing rotate_points_to_x_axis (returns Q,U given PD,PA)', test_q, test_u)
 
     assert np.allclose([average_mu, mdp99, Pol_frac, Pol_angl], [0.19, 0.22, 185, 82], atol=[0.1, 0.1, 5, 10])
