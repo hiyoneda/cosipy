7542381d16a79984f21e60e5e3e2802f2766e327
Hiroki Yoneda <hiroki.yoneda.phys@gmail.com>
2026-02-20 21:17:37 +0900

    Added accel_bkg_norm to MaxStepAccelerator

--- cosipy/image_deconvolution/max_step_accelerator.py
+++ cosipy/image_deconvolution/max_step_accelerator.py
@@ -23,6 +23,7 @@ class MaxStepAccelerator(AcceleratorBase):
 
     algorithm: MaxStep
     accel_factor_max: 10.0
+    accel_bkg_norm: False
     """
 
     n_em_steps_required = 1
@@ -30,6 +31,7 @@ class MaxStepAccelerator(AcceleratorBase):
     def __init__(self, parameter):
         super().__init__(parameter)
         self.accel_factor_max = float(parameter.get("accel_factor_max", DEFAULT_ACCEL_FACTOR_MAX))
+        self.accel_bkg_norm = bool(parameter.get("accel_bkg_norm", False))
         logger.info(f"[MaxStepAccelerator] accel_factor_max={self.accel_factor_max}")
 
     def compute(
@@ -48,18 +50,20 @@ class MaxStepAccelerator(AcceleratorBase):
 
         if accel_factor > 1.0:
             new_model = before.model + delta_model * accel_factor
-            new_dict_bkg_norm = after.dict_bkg_norm
-#            new_dict_bkg_norm = {
-#                key: before.dict_bkg_norm[key] + (after.dict_bkg_norm[key] - before.dict_bkg_norm[key]) * accel_factor
-#                for key in before.dict_bkg_norm
-#            }
+            if self.accel_bkg_norm:
+                new_dict_bkg_norm = {
+                    key: before.dict_bkg_norm[key] + (after.dict_bkg_norm[key] - before.dict_bkg_norm[key]) * accel_factor
+                    for key in before.dict_bkg_norm
+                }
+            else:
+                new_dict_bkg_norm = after.dict_bkg_norm
 
             # LH check: reuse source expectation, recompute bkg only
             source_expectation_list = [
                 src_before + (src_after - src_before) * accel_factor
                 for src_before, src_after in zip(before.source_expectation_list, after.source_expectation_list)
             ]
-            bkg_expectation_list = after.bkg_expectation_list
+            bkg_expectation_list = dataset.calc_bkg_expectation_list(new_dict_bkg_norm)
             expectation_list     = dataset.combine_expectation_list(source_expectation_list, bkg_expectation_list)
 
             ll_accel = np.sum(dataset.calc_log_likelihood_list(expectation_list))
